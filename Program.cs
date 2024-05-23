26c88a82f75598abba90dce50458b0a286d9e617
Vinicius Cunha <ViCunha@outlook.com>
2024-05-23 23:30:59 +0100

    message

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+// connection string to your Redis Cache    
+string connectionString = "";
+
+const string KEY = "test:key";
+
+Console.WriteLine(">> Begin");
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString) )
+{
+    IDatabase db = cache.GetDatabase();
+
+     // Snippet below executes a PING to test the server connection
+     var result = await db.ExecuteAsync("ping");
+     Console.WriteLine(result.ToString());
+
+     // Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+     bool setValue = await db.StringSetAsync(KEY, "100");
+     Console.WriteLine($"SET: {setValue}");
+
+    string getValue = await db.StringGetAsync(KEY); 
+    Console.WriteLine($"GET: {getValue}");
+}
+
+Console.WriteLine(">> End");
